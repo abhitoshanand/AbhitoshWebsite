9d0974fbb8802c220edc1015fbf0f62e70a86513
abhitoshanand <110685657+abhitoshanand@users.noreply.github.com>
2024-12-13 23:26:09 +0530

    Update testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -12,10 +12,10 @@ export const testimonialData = [
     text: "Sir had been an amazing teacher! He made physics so easy to understand and cleared every single doubt I had. The way he made me practice really worked—I could remember all the textbook answers by the end of the year! Beyond that, he was like a big brother to me, always listening to my endless chatter while keeping me on track with my syllabus and assignments. Thank you for being so supportive and making learning such a positive experience!"
   },
   {
-    name: "Anjali Sharma",
-    grade: "Class X",
-    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=400",
-    text: "Thanks to sir's guidance, physics has become my favorite subject. His interactive teaching style and patience in clearing doubts are remarkable."
+    name: "Pratik Bharadwaj",
+    grade: "Class VIII, DAV NIT, Jamshedpur",
+    image: "/IMG-20241213-WA0010_Nero AI_Photo_Face.jpeg",
+    text: "Abhitosh Sir is a great physics teacher who has the profound knowledge of the subject, which has been equalled by the capability of simplifying complex ideas with such clarity and precision. The kind of teaching inspires curiosity and thus helps in instilling awe within the intricate details of physics. It is so patience yet engrossing that it gives everyone a sense of confidence while empowering them on the way to learning."
   },
   {
     name: "Arjun Patel",
