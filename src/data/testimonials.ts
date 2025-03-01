f449a6100373d5df6f2e2e1c69f3bf084c9c85a8
abhitoshanand <110685657+abhitoshanand@users.noreply.github.com>
2025-03-01 21:34:35 +0530

    Update testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -2,7 +2,7 @@ export const testimonialData = [
   {
     name: "Aadhya Singh",
     grade: "Class XII, Sheyn International School, Kandra",
-    image: "/IMG-20241210-WA0002.jpg",
+    image: "/IMG-20250301-WA0015_Nero_AI_Image_Upscaler_Photo_Face.jpeg",
     text: "When people say that a teacher can shape a student’s perspective, I truly believe it because of Abhitosh Sir. His ability to simplify complex physics concepts, along with his patience and dedication, made learning an enjoyable experience. He not only strengthened my understanding but also boosted my confidence in the subject. I am truly grateful for their guidance and highly recommend him to anyone seeking an excellent physics tutor."
   },
   {
