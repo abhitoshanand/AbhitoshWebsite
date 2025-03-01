980ab0d9dd872b6a1af27fa57a5381adacbfe208
abhitoshanand <110685657+abhitoshanand@users.noreply.github.com>
2025-03-01 21:21:23 +0530

    Update testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -1,9 +1,9 @@
 export const testimonialData = [
   {
-    name: "Raajvi Singh",
-    grade: "Class IX, J.H. Tarapore, Jamshedpur",
+    name: "Aadhya Singh",
+    grade: "Class XII, Sheyn International School",
     image: "/IMG-20241210-WA0002.jpg",
-    text: "Well what can I even say about him, he's litreally the best I don't remember the last time I had a doubt cause he explains every topic in such a detailed manner because of which every little concept is clear. He always tell the easiest way to solve any question which is very helpful. Many people don't like physics but trust me if he teaches you he would make it so easy. His dedication towards everyone is really inspiring."
+    text: "When people say that a teacher can shape a student’s perspective, I truly believe it because of Abhitosh Sir. His ability to simplify complex physics concepts, along with his patience and dedication, made learning an enjoyable experience. He not only strengthened my understanding but also boosted my confidence in the subject. I am truly grateful for their guidance and highly recommend him to anyone seeking an excellent physics tutor."
   },
   {
     name: "Arya Jha",
@@ -17,5 +17,10 @@ export const testimonialData = [
     image: "/IMG-20241213-WA0010_Nero AI_Photo_Face.jpeg",
     text: "Abhitosh Sir is a great physics teacher who has the profound knowledge of the subject, which has been equalled by the capability of simplifying complex ideas with such clarity and precision. The kind of teaching inspires curiosity and thus helps in instilling awe within the intricate details of physics. It is so patience yet engrossing that it gives everyone a sense of confidence while empowering them on the way to learning."
   },
-  
+  {
+    name: "Raajvi Singh",
+    grade: "Class IX, J.H. Tarapore, Jamshedpur",
+    image: "/IMG-20241210-WA0002.jpg",
+    text: "Well what can I even say about him, he's litreally the best I don't remember the last time I had a doubt cause he explains every topic in such a detailed manner because of which every little concept is clear. He always tell the easiest way to solve any question which is very helpful. Many people don't like physics but trust me if he teaches you he would make it so easy. His dedication towards everyone is really inspiring."
+  }
 ];
